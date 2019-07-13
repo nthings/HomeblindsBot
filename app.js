fd62848a43fd176ea9daf742f480bb6c95c0b7ce
Mauricio Martinez <n_othing@hotmail.com>
2019-07-13 17:33:51 -0500

    Restarting the dev

--- app.js
+++ app.js
@@ -1,190 +1,105 @@
-'use strict'
+'use strict';
+require('dotenv').config()
+const BootBot = require('bootbot');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
-
-// parse application/json
-app.use(bodyParser.json());
-
-// index
-app.get('/', function (req, res) {
-	res.send('hello world i am a secret bot');
-})
-
-// for facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge']);
-	} else {
-		res.send('Error, wrong token');
-	}
-})
-
-// to post data
-var getAlto = false, getAncho = false, alto = 0, ancho = 0, persiana = "", precio=100;
-app.post('/webhook/', function (req, res) {
-	let messaging_events = req.body.entry[0].messaging;
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = req.body.entry[0].messaging[i];
-		let sender = event.sender.id;
-		if (event.message && event.message.text) {
-			let text = event.message.text;
-			if (text.toUpperCase().match(/(COT)|(PRE)+/g)){ 
-				console.log("MATCH");
-				sendTextMessage(sender, "Claro!, aqui puedes ver nuestro catalogo de persianas. Elige la que mas te guste 😀");
-				sendPersianas(sender);
-				continue;
-			}
-
-			console.log(text);
-			if(getAncho && ancho == 0 && !isNaN(text)){
-				console.log("IM IN ANCHO");
-				ancho = text.match(/(?:\d*\.)?\d+/);
-				getAncho = false;
-				sendTextMessage(sender, "Precio del metro cuadrado de la persiana " + persiana + ": $" + precio + 
-					" . Segun las medidas que nos diste (" + alto + " mts. X " + ancho + " mts.) Tu persiana costaria: $"+(precio*(alto*ancho)));
-				continue;
-			}
-
-			if(getAlto && alto == 0 && text.substr(0,text.indexOf(' ')) != "Necesitamos"){
-				console.log("IM IN ALTO");
-				alto = text.match(/(?:\d*\.)?\d+/);
-				getAlto = false;
-
-				getAncho = true;
-				sendTextMessage(sender, "Excelente!, ahora ingresa el ancho ↔️ de tu ventana. En metros");			
-				continue;
-			}
-			
-
-			//sendTextMessage(sender, "Text received, echo: "+text.substring(0, 200));
-		}
-		if (event.postback) {
-			// let text = JSON.stringify(event.postback);
-			if(event.postback.title === "COTIZAR"){
-				persiana = event.postback.payload;
-				sendTextMessage(sender, "Necesitamos las medidas de tu ventana 📐. Ingresa el alto ↕️ de tu ventana. En metros");
-				getAlto = true;
-			}
-			// sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
-			continue;
-		}
-	}
-	res.sendStatus(200);
+const bot = new BootBot({
+  accessToken:  process.env.FB_ACCESS_TOKEN,
+  verifyToken: process.env.FB_VERIFY_TOKEN,
+  appSecret:  process.env.FB_APP_SECRET
 });
 
+bot.setGreetingText('Hey there! Welcome to BootBot!');
+bot.setGetStartedButton((payload, chat) => {
+  chat.say('Buen día, gracias por permitirnos decorar la luz de sus ventanas. Eliga una opción para continuar:');
+});
+bot.setPersistentMenu([
+  {
+    type: 'postback',
+    title: 'Cotizar',
+    payload: 'COTIZAR'
+  },
+  {
+    type: 'postback',
+    title: 'Enviar Mensaje',
+    payload: 'SEND_MESSAGE'
+  },
+  {
+    type: 'postback',
+    title: 'Contacto',
+    payload: 'DISPLAY_CONTACT'
+  }
+]);
 
-// recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.FB_PAGE_ACCESS_TOKEN
-// TOKEN FOR MAIN PAGE
-//const token = "EAACB3xHMg7cBAIcAH11IZBbuQvI3CgZCMEhqR4sGNqmGvWqMVbTQ9JfS4ss1pwCMIU3GNVLUzDNmbZCczd6qy0ZCAykLfwftdZBKu0fT5OdNMIGqv5ZBoHjZArMMigthadQbTMYZAmXKK5vUY4NRj79EDP3p5twqGNdzYv3m7uiw7wZDZD"
+bot.hear([
+  'cotizacion',
+  'cotización',
+  'cotisacion',
+  'cotisasion',
+  'cotizar',
+  'cotisar',
+  'precio',
+  'presio',
+], (payload, chat) => {
+  chat.conversation(async (convo) => {
+    await convo.sendTypingIndicator(1000);
+    await launchCotizacion(convo);
+  });
+});
 
-//TOKEN FOR TEST PAGE
-const token = "EAACB3xHMg7cBABc2XfWiKBpqvwZCSDgdLHIjKlLFHYfzv9XcXumWJs0RnXn7pgMZA3WEXmEw04IrSuxg9UBn48Sr6fSCenAdZBRokmsRwSu1wd8ic508b0XJwR3hWZBlQuhWm6thxw5MXYi49gRaUjXag4K9LZARoSd2ylFrrGwZDZD"
-function sendTextMessage(sender, text) {
-	let messageData = { text:text }
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
+bot.on('postback:COTIZAR', (payload, chat) => {
+  chat.conversation(async (convo) => {
+    await convo.sendTypingIndicator(1000);
+    await launchCotizacion(convo);
+  });
+});
+
+bot.on('postback:COTIZAR', (payload, chat) => {
+  chat.conversation(async (convo) => {
+    await convo.sendTypingIndicator(1000);
+    await launchCotizacion(convo);
+  });
+});
+
+bot.on('postback:SEND_MESSAGE', (payload, chat) => {
+  chat.say(`Envía tu mensaje y en unos momentos te estaremos respondiendo :)`);
+});
+
+bot.on('postback:DISPLAY_CONTACT', (payload, chat) => {
+  chat.say(`En construcción`);
+});
+
+const launchCotizacion = async (convo) => {
+  convo.ask((convo) => {
+    const buttons = [
+      { type: 'postback', title: 'Enrollable', payload: 'ENROLLABLE' },
+      { type: 'postback', title: 'Sheer', payload: 'SHEER' },
+      { type: 'postback', title: 'Panel Japonés', payload: 'PANEL' }
+    ];
+    convo.sendButtonTemplate(`Elige un tipo de persiana para comenzar tu cotización:`, buttons);
+  }, (payload, convo, data) => {
+    const text = payload.message.text;
+    convo.set('gender', text);
+    convo.say(`Great, you are a ${text}`).then(() => askAge(convo));
+  }, [
+      {
+        event: 'postback:ENROLLABLE',
+        callback: (payload, convo) => {
+          convo.say('Elegiste persiana enrollable');
+        }
+      },
+      {
+        event: 'postback:SHEER',
+        callback: (payload, convo) => {
+          convo.say('Elegiste persiana sheer');
+        }
+      },
+      {
+        event: 'postback:PANEL',
+        callback: (payload, convo) => {
+          convo.say('Elegiste panel japonés');
+        }
+      },
+    ]);
 };
 
-function sendPersianas(sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "list",
-				"top_element_style": "compact",
-				"elements": [{
-					"title": "Enrollable",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://www.persianastannah.com.mx/images/persianas-enrollables-en-monterrey-1.jpg",
-					"default_action":{
-						"type": "web_url",
-						"url": "https://www.google.com",
-						"webview_height_ratio": "tall",
-						"fallback_url":"https://www.google.com",
-						"messenger_extensions": true
-					},
-					"buttons": [{
-						"title": "COTIZAR",
-						"type": "postback",
-						"payload": "enrollable"
-					}],
-				}, {
-					"title": "Sheer",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://www.servipersianas.com.mx/sheerelegance04.jpg",
-					"default_action":{
-						"type": "web_url",
-						"url": "https://www.google.com",
-						"webview_height_ratio": "tall",
-						"fallback_url":"https://www.google.com",
-						"messenger_extensions": true
-					},
-					"buttons": [{
-						"type": "postback",
-						"title": "COTIZAR",
-						"payload": "sheer"
-					}],
-				},{
-					"title": "Vertical",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://espacioflex.com/wp-content/uploads/2015/09/04-persiana-vertical.jpg",
-					"default_action":{
-						"type": "web_url",
-						"url": "https://www.google.com",
-						"webview_height_ratio": "tall",
-						"fallback_url":"https://www.google.com",
-						"messenger_extensions": true
-					},
-					"buttons": [{
-						"type": "postback",
-						"title": "COTIZAR",
-						"payload": "sheer"
-					}],
-				}]
-			}
-		}
-	};
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error);
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error);
-		}
-	});
-};
-
-// spin spin sugar
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'));
-})
+bot.start();
